62f4023d92f78b700eeb58190407bc08da1b9798
Nathan979 <simtrai@gmail.com>
2014-04-02 08:49:34 +0200

    Completo

--- Vendors.php
+++ Vendors.php
@@ -10,12 +10,12 @@
 $languageStrings = array(
 	'Vendors'                      => 'Fornitori'                   , 
 	'SINGLE_Vendors'               => 'Fornitore'                   , 
-	'LBL_ADD_RECORD'               => 'Add Vendor'                  , 
-	'LBL_RECORDS_LIST'             => 'Vendors List'                , 
-	'LBL_VENDOR_INFORMATION'       => 'Informazioni Fornitore:'     , 
-	'LBL_VENDOR_ADDRESS_INFORMATION' => 'Informazioni Indirizzo:'     , 
+	'LBL_ADD_RECORD'               => 'Aggiungi Fornitore'                  , 
+	'LBL_RECORDS_LIST'             => 'Lista Fornitori'                , 
+	'LBL_VENDOR_INFORMATION'       => 'Informazioni Fornitore'     , 
+	'LBL_VENDOR_ADDRESS_INFORMATION' => 'Informazioni Indirizzo'     , 
 	'Vendor Name'                  => 'Nome Fornitore'              , 
-	'Vendor No'                    => 'No. Fornitore'               , 
+	'Vendor No'                    => 'Nr. Fornitore'               , 
 	'Website'                      => 'Sito Web'                    , 
 	'GL Account'                   => 'Codice Contabile'            , 
 	'300-Sales-Software'           => '300-Vendita-Software'        , 
@@ -29,7 +29,7 @@ $languageStrings = array(
 	'308-Sales-Books'              => '308-Vendite-Libri'           , 
 );
 $jsLanguageStrings = array(
-	'LBL_RELATED_RECORD_DELETE_CONFIRMATION' => 'Are you sure you want to delete?', 
-	'LBL_DELETE_CONFIRMATION'      => 'Deleting this Vendor will remove its related PurchaseOrders. Are you sure you want to delete this Vendor?', 
-	'LBL_MASS_DELETE_CONFIRMATION' => 'Deleting this vendor(s) will remove its related Purchase Orders. Are you sure you want to delete the selected Records?', 
+	'LBL_RELATED_RECORD_DELETE_CONFIRMATION' => 'Sei sicuro di voler cancellare?', 
+	'LBL_DELETE_CONFIRMATION'      => 'Cancellando questo Fornitore cancellerai i relativi ordini di acquisto. Sei sicuro di voler cancellare questo Fornitore?', 
+	'LBL_MASS_DELETE_CONFIRMATION' => 'Cancellando questi Fornitori cancellerai i relativi ordini di acquisto. Sei sicuro di voler cancellare?', 
 );
