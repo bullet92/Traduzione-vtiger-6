d05aa50eea8db4bab0727988f89b8b67b4e4e7a9
barra <barra78@gmail.com>
2014-04-11 18:35:37 +0200

    Update Emails.php

--- Emails.php
+++ Emails.php
@@ -8,31 +8,31 @@
  * All Rights Reserved.
  ************************************************************************************/
 $languageStrings = array(
-	'SINGLE_Emails'                => 'Email'                       , // TODO: Review
-	'Emails'                       => 'Email'                       , // TODO: Review
+	'SINGLE_Emails'                => 'Email'                       , 
+	'Emails'                       => 'Email'                       , 
 	'LBL_SELECT_EMAIL_IDS'         => 'Seleziona indirizzo email'   , 
 	'LBL_SUBJECT'                  => 'Soggetto'                    , 
 	'LBL_ATTACHMENT'               => 'Allegato'                    , 
 	'LBL_BROWSE_CRM'               => 'Cerca nel CRM'               , 
 	'LBL_SEND'                     => 'Invia'                       , 
 	'LBL_SAVE_AS_DRAFT'            => 'Salva come bozza'            , 
-	'LBL_GO_TO_PREVIEW'            => 'Vai all\'anteprima'          , // TODO: Review
+	'LBL_GO_TO_PREVIEW'            => 'Vai all&acute;anteprima'     , 
 	'LBL_SELECT_EMAIL_TEMPLATE'    => 'Seleziona un Template Email' , 
 	'LBL_COMPOSE_EMAIL'            => 'Componi e-mail'              , 
-	'LBL_TO'                       => 'To'                          , // TODO: Review
+	'LBL_TO'                       => 'To'                          ,
 	'LBL_CC'                       => 'CC:'                         , 
 	'LBL_BCC'                      => 'CCN:'                        , 
 	'LBL_ADD_CC'                   => 'Aggiungi Cc'                 , 
 	'LBL_ADD_BCC'                  => 'Aggiungi Bcc'                , 
-	'LBL_MAX_UPLOAD_SIZE'          => 'La dimensione massima per upload è'      , 
+	'LBL_MAX_UPLOAD_SIZE'          => 'La dimensione massima per upload &egrave;' , 
 	'LBL_EXCEEDED'                 => 'Eccede'                      ,  
-	'LBL_FORWARD'                  => 'Inoltra'                     , // TODO: Review
-	'LBL_PRINT'                    => 'Stampa'                      , // TODO: Review
-	'LBL_DESCRIPTION'              => 'Descrizione'                 , // TODO: Review
-	'LBL_FROM'                     => 'Da'                          , // TODO: Review
-	'LBL_INFO'                     => 'Info'                        , // TODO: Review
-	'LBL_DRAFTED_ON'               => 'Bozza in'                    , // TODO: Review
-	'LBL_SENT_ON'                  => 'Inviato a'                   , // TODO: Review
-	'LBL_OWNER'                    => 'Proprietario'                , // TODO: Review
-	'Date & Time Sent'             => 'Data di invio'               , // TODO: Review
+	'LBL_FORWARD'                  => 'Inoltra'                     ,
+	'LBL_PRINT'                    => 'Stampa'                      ,
+	'LBL_DESCRIPTION'              => 'Descrizione'                 ,
+	'LBL_FROM'                     => 'Da'                          ,
+	'LBL_INFO'                     => 'Info'                        ,
+	'LBL_DRAFTED_ON'               => 'Bozza in'                    ,
+	'LBL_SENT_ON'                  => 'Inviato a'                   ,
+	'LBL_OWNER'                    => 'Proprietario'                ,
+	'Date & Time Sent'             => 'Data di invio'               ,
 );
