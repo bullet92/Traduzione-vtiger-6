6430d52e5c8dff2268d42498c22e938f91741d2c
barra <barra78@gmail.com>
2014-04-12 15:17:42 +0200

    Update RecycleBin.php

--- RecycleBin.php
+++ RecycleBin.php
@@ -8,15 +8,15 @@
  * All Rights Reserved.
  ************************************************************************************/
 $languageStrings = array(
-	'RecycleBin'                   => 'Cestino'                 , // TODO: Review
-	'LBL_SELECT_MODULE'            => 'Seleziona Modulo'               , // TODO: Review
-	'LBL_EMPTY_RECYCLEBIN'         => 'Svuota Cestino'           , // TODO: Review
-	'LBL_RESTORE'                  => 'Ripristina'                     , // TODO: Review
-	'LBL_NO_PERMITTED_MODULES'     => 'Nessun modulo consentito disponibile', // TODO: Review
-	'LBL_RECORDS_LIST'             => 'Lista records cestino'            , // TODO: Review
-	'LBL_NO_RECORDS_FOUND'         => 'Nessun record trovato nel modulo Ripristina', // TODO: Review
+	'RecycleBin'                   => 'Cestino'                 ,
+	'LBL_SELECT_MODULE'            => 'Seleziona modulo'        ,
+	'LBL_EMPTY_RECYCLEBIN'         => 'Svuota cestino'          ,
+	'LBL_RESTORE'                  => 'Ripristina'              , 
+	'LBL_NO_PERMITTED_MODULES'     => 'Nessun modulo consentito disponibile', 
+	'LBL_RECORDS_LIST'             => 'Lista records cestino'   ,
+	'LBL_NO_RECORDS_FOUND'         => 'Nessun record trovato'   ,
 );
 $jsLanguageStrings = array(
-	'JS_MSG_EMPTY_RB_CONFIRMATION' => 'Sei sicuro di voler rimuovere definitivamente tutti i record cancellati?', // TODO: Review
-	'JS_LBL_RESTORE_RECORDS_CONFIRMATION' => 'Sei sicuro di volere ripristinare i record?', // TODO: Review
+	'JS_MSG_EMPTY_RB_CONFIRMATION' => 'Sei sicuro di voler rimuovere definitivamente tutti i record cancellati?',
+	'JS_LBL_RESTORE_RECORDS_CONFIRMATION' => 'Sei sicuro di volere ripristinare i record?', 
 );
