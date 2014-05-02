f1e44ef0c7e35d502f0ea8da637b86662427e658
barra <barra78@gmail.com>
2014-05-02 22:17:54 +0200

    Caricato e tradotto

---
+++ Google.php
@@ -0,0 +1,37 @@
+<?php
+
+/*+***********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ *************************************************************************************/
+    $languageStrings = array(
+        'Map' => 'Mappa',
+        'EXTENTIONNAME' => 'Google',
+        'LBL_UPDATES_CRM' => 'Aggiornamenti sul CRM',
+        'LBL_UPDATES_GOOGLE' => 'Aggiornamenti su Google',
+        'LBL_UPDATED' => 'Aggiornato',
+        'LBL_ADDED' => 'Aggiunto',
+        'LBL_DELETED' => 'Cancellato',
+        'LBL_SYNCRONIZED' => 'Sincronizzato',
+        'LBL_NOT_SYNCRONIZED' => 'Non ancora sincronizzato',
+        'LBL_SYNC_BUTTON' => 'Sincronizza ora',
+        'LBL_MORE_VTIGER' => 'Ci sono molti elementi da sincronizzare in vtiger',
+        'LBL_MORE_GOOGLE' => 'Ci sono molti elementi da sincronizzare su Google',
+        'Contact Name'=>'Nome contatto',
+        'Email'=>'Email',
+        'Mobile Phone'=>'Cellulare',
+        'Address' => 'Indirizzo',
+        'Event Title'=>'Titolo evento',
+        'Start Date'=>'Data inizio',
+        'Until Date'=>'Fino a',
+        'Description'=>'Descrizinoe'
+    );
+    $jsLanguageStrings = array(
+        'LBL_SYNC_BUTTON' => 'Sincronizza ora',
+        'LBL_SYNCRONIZING' => 'In sincronizzazione....',
+        'LBL_NOT_SYNCRONIZE' => 'Non ancora sincronizzato'
+    );
